6eb3510585c2bad9420e60c36486e3165a5d75db
lucasbotteri <lucasbotteri@hotmail.com>
2015-05-25 18:23:38 -0300

    mejorada implementacion login

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,8 @@ router.get('/', function(req, res, next) {
 	// si la sesion no tiene un atributo que sea usuario es porque no tiene una sesion activa
 	//entonces pongo sesionUsuario en null para que en la barra solo aparezca iniciar sesion y registarse
 	//en caso de que tenga una sesion activa la barra aparecera con el nombre de usuario, notif y cerrar sesion
-	var sesionUsuario = null
-	if (req.session.hasOwnProperty('usuario')){
-		sesionUsuario = req.session.usuario;
-	}
 	dbPublicacion.getPublicaciones(function(error, resultado){
-		res.render('index', {publicaciones : resultado, sesionUsuario : sesionUsuario});
+		res.render('index', {publicaciones : resultado, sesionUsuario : req.session.usuario});
 	});
 });
 
@@ -40,9 +36,9 @@ router.post('/buscar', function(req, res, next){
 	}
 	dbPublicacion.getPublicacionesByNombre(req.body.nombrePublicacion, function(error, resultado){
 		if (error) {
-			res.render('error', { mensaje:'Hubo un error en la búsqueda, por favor intente de nuevo', sesionUsuario:sesionUsuario })
+			res.render('error', { mensaje:'Hubo un error en la búsqueda, por favor intente de nuevo', sesionUsuario:req.session.usuario })
 		} else{
-			res.render('index', { publicaciones:resultado, sesionUsuario:sesionUsuario });
+			res.render('index', { publicaciones:resultado, sesionUsuario:req.session.usuario });
 		};
 	});
 });
