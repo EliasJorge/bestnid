805effae66d5e7a40a72e76e7f8810d438f5982f
lucasbotteri <lucasbotteri@hotmail.com>
2015-06-19 13:32:01 -0300

    arreglado privilegios de publicacion

--- schema.sql
+++ schema.sql
@@ -108,6 +108,7 @@ CREATE TABLE `publicacion` (
   `fechaFin` date NOT NULL,
   `visible` tinyint(1) NOT NULL DEFAULT 1,
   `terminada` tinyint(1) NOT NULL DEFAULT 0,
+  `pagada` tinyint(1) NOT NULL DEFAULT 0,
   `idOfertaGanadora` int(11) DEFAULT NULL,
   `idCategoria` int(11) NOT NULL,
   `idUsuario` int(11) NOT NULL,
