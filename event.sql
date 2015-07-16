1994a559a9dfc43a2d3158c36931a26163f50880
Juanma Coria <juanmc2005@hotmail.com>
2015-07-16 01:01:22 -0300

    Ahora se notifica a un publicador cuando su publicacion finaliza

--- event.sql
+++ event.sql
@@ -1,9 +1,9 @@
 SET GLOBAL event_scheduler = ON;
 
 create procedure actualizacionBestnid() 
-  update publicacion 
-  set terminada = true
-  where terminada = false and fechaFin <= curdate();
+  update publicacion, usuario
+  set publicacion.terminada = true, usuario.tieneNotificaciones = 1
+  where publicacion.terminada = false and publicacion.fechaFin <= curdate() and usuario.idUsuario = publicacion.idUsuario;
 
 CREATE EVENT IF NOT EXISTS actualizadorBestnid 
 ON SCHEDULE EVERY 1 DAY
