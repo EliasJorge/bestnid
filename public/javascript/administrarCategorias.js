cb08d746ca4e4355088e8c30912f67ce5cef60d2
Juanma Coria <juanmc2005@hotmail.com>
2015-07-16 21:14:12 -0300

    Ahora la validacion del nombre de categoria en agregar categoria se maneja con ajax

--- public/javascript/administrarCategorias.js
+++ public/javascript/administrarCategorias.js
@@ -35,6 +35,39 @@ $(document).ready(function(){
         });
     }
 
+    var validarSiExisteNombreAgregar = function(event){
+        // cancels the form submission
+        event.preventDefault();
+        //Me fijo si eexiste el nombre de esta categoria
+        $.get("/existeCategoria/" + $('#nombreCategoria').val(), function(data, status){
+            if (data != 'dbError') {
+                if (data == false) {
+                    //ya hay una con ese nombre
+                    if ($(".sacar").length == 0) {
+                        $("#formAgregarBody").after('<div class="col-md-12 sacar"><div class="alert alert-danger"><span>Ya existe una categoria con este nombre</span></div></div>');
+                        setTimeout(function() {
+                            $(".sacar").slideUp('fast', function(){
+                                $(".sacar").remove();
+                            });
+                        }, 3000);
+                    };
+                } else {
+                    //unbind y submit
+                    $("#formAgregar").unbind("submit").submit();
+                };
+            } else {
+                if ($(".sacar").length == 0) {
+                    $("#formAgregarBody").after('<div class="col-md-12 sacar"><div class="alert alert-danger"><span>Ocurrio un error, por favor intente de nuevo</span></div></div>');
+                    setTimeout(function() {
+                        $(".sacar").slideUp('fast', function(){
+                            $(".sacar").remove();
+                        });
+                    }, 3000);
+                };
+            };
+        });
+    }
+
     var pasarParametrosAModelModificar = function (event) {
         var boton= event.relatedTarget; // Button that triggered the modal
         var idCategoria = boton.getAttribute("data-idCategoria");
@@ -49,4 +82,5 @@ $(document).ready(function(){
 
     $('#modalModificar').on('show.bs.modal', pasarParametrosAModelModificar);
     $('#formModificar').bind("submit", validarSiExisteNombre);
+    $('#formAgregar').bind("submit", validarSiExisteNombreAgregar);
 });
